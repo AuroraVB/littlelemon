b9353728037a5ac4be60d032107741af4ae91ef4
Aurora Valencia <lilianaurora.valenciabernal8@gmail.com>
2025-03-06 17:43:30 -0600

    commit done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Nav.css"; 
+import logo from "../images/logo.png"
 
 function Nav(){
     return(
         <nav>
+            <a href="#home">
+            <img src={logo} alt="logo" weight={202} height={76}></img>
+            </a>
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
