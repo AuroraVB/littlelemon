1cb57256969179d40d233450ca9231099f9547fa
Aurora Valencia <lilianaurora.valenciabernal8@gmail.com>
2025-03-12 18:59:41 -0600

    commit done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Booking from './components/Booking';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav/>
-      <Main/>
+
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/Booking" element={<Booking/>}/>
+      </Routes>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./Nav.css"; 
 import logo from "../images/logo.png"
+import { Link } from "react-router-dom";
 
-function Nav(){
-    return(
-        <nav>
-            <a href="#home">
-            <img src={logo} alt="logo" weight={202} height={76}></img>
-            </a>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="#reservations">Reservations</a></li>
-                <li><a href="#order">Order Online</a></li>
-                <li><a href="#login">Login</a></li>
-            </ul>
-        </nav>
-    )
-}
+function Nav() {
+    return (
+      <nav>
+        <Link to="/">
+          <img src={logo} alt="logo" width={202} height={76} />
+        </Link>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/menu">Menu</Link></li>
+          <li><Link to="/reservations">Reservations</Link></li>
+          <li><Link to="/order">Order Online</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+    );
+  }
 
 export default Nav; 
